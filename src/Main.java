5041ed716cf4f4e43760d9ca63bba1dbae0218a1
ak11234 <akpc556@gmail.com>
2016-06-28 17:03:49 -0500

    Working picalc!!!!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        double incircle=0,insqaure=0;
+
+        Scanner scan = new Scanner(System.in);
+        int dartamt;
+        System.out.print("Amount of darts: ");
+        dartamt = scan.nextInt();
+
+        for (int k=0 ; k < dartamt ; k++) {
+            double dartx,darty,distance;
+            dartx=Math.random()*2.0;
+            darty=Math.random()*2.0;
+            distance = Math.hypot(dartx-1.0, darty-1.0);
+            if (Math.abs(distance)<=1.0){
+                incircle++;
+                insqaure++;
+            } else {
+                insqaure++;
+            }
+
+
+        }
+        double picalc=(incircle/insqaure)*4;
+        System.out.println(picalc);
+
+
+
     }
 }
